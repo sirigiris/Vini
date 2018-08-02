e7e9ecae85cdd9471ecd5f846861d8504093957d
Naren Sirigiri <sirigiris@gmail.com>
2018-08-01 22:25:52 -0400

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path=require('path');
+const express=require('express');
+
+
+const publicpath=path.join(__dirname,'../public');
+const port=process.env.PORT|| 3000;
+var app=express();
+
+app.use(express.static(publicpath));
+
+app.listen(port,()=>{
+
+console.log(`Server is up on port ${port}`);
+
+});
