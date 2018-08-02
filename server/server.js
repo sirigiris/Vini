1c7dde523849588460bd1568b520948541d17f80
Naren Sirigiri <sirigiris@gmail.com>
2018-08-01 22:56:47 -0400

    second commit

--- server/server.js
+++ server/server.js
@@ -1,15 +1,27 @@
 const path=require('path');
+const http=require('http');
 const express=require('express');
-
+const socketio=require('socket.io');
 
 const publicpath=path.join(__dirname,'../public');
 const port=process.env.PORT|| 3000;
 var app=express();
 
+var server=http.createServer(app);
+var io=socketio(server);
+
 app.use(express.static(publicpath));
 
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+    console.log('new user connected');
 
-console.log(`Server is up on port ${port}`);
+    socket.on('disconnect',()=>{
+    console.log('disconnected from client');
+})
 
 });
+
+server.listen(port,()=>{
+    console.log(`Server is up on port ${port}`);
+});
+
