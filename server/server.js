75cd08aa65991c069070d35de4b1435679579e2a
Naren Sirigiri <sirigiris@gmail.com>
2018-08-06 20:27:01 -0400

    add newmessage and createmessage events

--- server/server.js
+++ server/server.js
@@ -17,8 +17,26 @@ io.on('connection',(socket)=>{
 
     socket.on('disconnect',()=>{
     console.log('disconnected from client');
-})
+    })
+    socket.emit('newEmail',{
+        from: 'hema@example.com',
+        text: 'hi',
+        createAt: 123
+    });
 
+    socket.emit('newMessage',{
+        from: 'naren@example.com',
+        text: 'new message sent',
+        createAt: '08/03/2018 9:00PM'
+    });
+
+    socket.on('createEmail', function(newemail){
+        console.log('createEmail:',newemail);
+    })
+
+    socket.on('createMessage', function(message){
+        console.log("createMessage:",message )
+    })
 });
 
 server.listen(port,()=>{
